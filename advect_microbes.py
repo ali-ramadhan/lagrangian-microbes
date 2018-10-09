4f64a16e2f9da5d73cf6e97e948b5fd312f8489e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-09 11:15:47 -0400

    Loaded subset of OSCAR velocity field.

--- advect_microbes.py
+++ advect_microbes.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import timedelta
 
 import numpy as np
 import xarray as xr
@@ -15,3 +15,19 @@ velocity_subdataset = velocity_dataset.sel(time=np.datetime64('2017-01-01'), yea
 print(velocity_subdataset)
 
 print(velocity_subdataset['u'])
+
+lons = velocity_subdataset['longitude'].values
+lats = velocity_subdataset['latitude'].values
+
+u_field = parcels.field.Field(name='u', data=velocity_subdataset['u'].values, lon=lons, lat=lats)
+v_field = parcels.field.Field(name='v', data=velocity_subdataset['v'].values, lon=lons, lat=lats)
+
+fieldset = parcels.fieldset.FieldSet(u_field, v_field)
+fieldset.U.show()
+
+pset = parcels.ParticleSet.from_list(fieldset=fieldset, pclass=parcels.JITParticle, lon=[-170, -160, -150, -140, -130], lat=[10, 20, 30, 40, 50])
+print(pset)
+
+pset.show(field=fieldset.V)
+
+pset.execute(parcels.AdvectionRK4, runtime=timedelta(days=1), dt=timedelta(minutes=1), output_file=pset.ParticleFile(name="advected_microbes.nc", outputdt=timedelta(minutes=10)))

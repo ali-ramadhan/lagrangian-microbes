c1442024eb9a710b70b17e3c52569bc7b53c576c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-05 18:55:14 -0500

    Refactored parcels advection.

--- advect_microbes.py
+++ advect_microbes.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import numpy as np
 import xarray as xr
@@ -11,23 +11,28 @@ velocity_dataset = xr.open_dataset(velocity_dataset_filepath)
 print(velocity_dataset)
 
 # Choose subset of velocity field we want to use
-velocity_subdataset = velocity_dataset.sel(time=np.datetime64('2017-01-01'), year=2017.0, depth=15.0, latitude=slice(60, 0), longitude=slice(-180, -120))
-print(velocity_subdataset)
+velocity_subdataset = velocity_dataset.sel(time=np.datetime64('2017-01-01'), year=2017.0, depth=15.0,
+    latitude=slice(60, 0), longitude=slice(-180, -120))
 
+print(velocity_subdataset)
 print(velocity_subdataset['u'])
 
-lons = velocity_subdataset['longitude'].values
 lats = velocity_subdataset['latitude'].values
+lons = velocity_subdataset['longitude'].values
+depth = np.array([15.0])
 
-u_field = parcels.field.Field(name='u', data=velocity_subdataset['u'].values, lon=lons, lat=lats)
-v_field = parcels.field.Field(name='v', data=velocity_subdataset['v'].values, lon=lons, lat=lats)
+u_data = velocity_subdataset['u'].values
+v_data = velocity_subdataset['v'].values
+
+u_field = parcels.field.Field(name='U', data=u_data,
+    lon=lons, lat=lats, depth=depth, mesh='spherical')
+v_field = parcels.field.Field(name='V', data=v_data,
+    lon=lons, lat=lats, depth=depth, mesh='spherical')
+
+u_magnitude = np.sqrt(u_data*u_data + v_data*v_data)
 
 fieldset = parcels.fieldset.FieldSet(u_field, v_field)
-fieldset.U.show()
+# fieldset.U.show()
 
-pset = parcels.ParticleSet.from_list(fieldset=fieldset, pclass=parcels.JITParticle, lon=[-170, -160, -150, -140, -130], lat=[10, 20, 30, 40, 50])
-print(pset)
-
-pset.show(field=fieldset.V)
-
-pset.execute(parcels.AdvectionRK4, runtime=timedelta(days=1), dt=timedelta(minutes=1), output_file=pset.ParticleFile(name="advected_microbes.nc", outputdt=timedelta(minutes=10)))
+lats_pset = np.tile(np.linspace(10, 50, 31), 31)
+lons_pset = np.repeat(np.linspace(-170, -130, 31), 31)

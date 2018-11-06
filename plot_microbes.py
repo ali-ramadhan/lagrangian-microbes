d975df8e35e5be9808d64485082d5c47ff79fcdb
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-06 09:10:41 -0500

    Plotting microbe warfare.

--- plot_microbes.py
+++ plot_microbes.py
@@ -1,3 +1,8 @@
+import pickle
+
+import numpy as np
+import xarray as xr
+
 import matplotlib
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -9,60 +14,83 @@ from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 from constants import N, t, dt, tpd, n_days
 
-vector_crs = ccrs.PlateCarree()
-land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '50m',
-    edgecolor='face',facecolor='dimgray', linewidth=0)
+for day in range(7):
+    velocity_dataset_filepath = '/home/alir/nobackup/data/oscar_third_deg_180/oscar_vel2017_180.nc'
+    velocity_dataset = xr.open_dataset(velocity_dataset_filepath)
+    velocity_subdataset = velocity_dataset.sel(time=np.datetime64('2017-01-01'), year=2017.0, depth=15.0,
+        latitude=slice(60, 0), longitude=slice(-180, -120))
+    
+    # Grid longitudes and latitudes.
+    glons = velocity_subdataset['longitude'].values
+    glats = velocity_subdataset['latitude'].values
+    depth = np.array([15.0])
 
-fig = plt.figure(figsize=(16, 9))
-matplotlib.rcParams.update({'font.size': 10})
+    u_data = velocity_subdataset['u'].values
+    v_data = velocity_subdataset['v'].values
 
-crs_sps = ccrs.PlateCarree(central_longitude=-150)
-crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
+    u_magnitude = np.sqrt(u_data*u_data + v_data*v_data)
 
-ax = plt.subplot(111, projection=crs_sps)
-ax.add_feature(land_50m)
-ax.set_extent([-180, -120, 0, 60], ccrs.PlateCarree())
+    for n in range(tpd):
+        pickle_filepath = "rps_microbe_species_d" + str(day).zfill(4) + "_n" + str(n).zfill(2) + ".pickle"
+        
+        with open(pickle_filepath, "rb") as f:
+            microbes = pickle.load(f)
 
-gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='black',
-    alpha=0.8, linestyle='--')
-gl.xlabels_top = False
-gl.ylabels_left = False
-gl.xlocator = mticker.FixedLocator([-180, -170, -160, -150, -140, -130, -120])
-gl.ylocator = mticker.FixedLocator([0, 10, 20, 30, 40, 50, 60])
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
+        # Microbe longitudes and latidues.
+        mlons, mlats, species = microbes[:, 0], microbes[:, 1], microbes[:, 2]
 
-im = ax.pcolormesh(lons, lats, u_magnitude, transform=vector_crs, vmin=0, vmax=1, cmap='Blues_r')
+        vector_crs = ccrs.PlateCarree()
+        land_50m = cartopy.feature.NaturalEarthFeature('physical', 'land', '50m',
+            edgecolor='face',facecolor='dimgray', linewidth=0)
 
-clb = fig.colorbar(im, ax=ax, extend='max', fraction=0.046, pad=0.1)
-clb.ax.set_title(r'm/s')
+        fig = plt.figure(figsize=(16, 9))
+        matplotlib.rcParams.update({'font.size': 10})
 
-rock_lats, rock_lons = [], []
-paper_lats, paper_lons = [], []
-scissors_lats, scissors_lons = [], []
+        crs_sps = ccrs.PlateCarree(central_longitude=-150)
+        crs_sps._threshold = 1000.0  # This solves https://github.com/SciTools/cartopy/issues/363
 
-for microbe in pset:
-    if microbe.species == 1:
-        rock_lats.append(microbe.lat)
-        rock_lons.append(microbe.lon)
-    elif microbe.species == 2:
-        paper_lats.append(microbe.lat)
-        paper_lons.append(microbe.lon)
-    elif microbe.species == 3:
-        scissors_lats.append(microbe.lat)
-        scissors_lons.append(microbe.lon)
+        ax = plt.subplot(111, projection=crs_sps)
+        ax.add_feature(land_50m)
+        ax.set_extent([-180, -120, 0, 60], ccrs.PlateCarree())
 
-ax.plot(rock_lons, rock_lats, marker='o', linestyle='', color='red', ms=4, label='Rocks', transform=vector_crs)
-ax.plot(paper_lons, paper_lats, marker='o', linestyle='', color='lime', ms=4, label='Papers', transform=vector_crs)
-ax.plot(scissors_lons, scissors_lats, marker='o', linestyle='', color='cyan', ms=4, label='Scissors', transform=vector_crs)
+        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='black',
+            alpha=0.8, linestyle='--')
+        gl.xlabels_top = False
+        gl.ylabels_left = False
+        gl.xlocator = mticker.FixedLocator([-180, -170, -160, -150, -140, -130, -120])
+        gl.ylocator = mticker.FixedLocator([0, 10, 20, 30, 40, 50, 60])
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
 
-plt.title(str(t))
-ax.legend()
+        im = ax.pcolormesh(glons, glats, u_magnitude, transform=vector_crs, vmin=0, vmax=1, cmap='Blues_r')
 
-# plt.show()
+        clb = fig.colorbar(im, ax=ax, extend='max', fraction=0.046, pad=0.1)
+        clb.ax.set_title(r'm/s')
 
-png_filename = "advected_microbes_" + str(n).zfill(4) + ".png"
-print("Saving figure: {:s}".format(png_filename))
-plt.savefig(png_filename, dpi=300, format='png', transparent=False)
+        rock_lats, rock_lons = [], []
+        paper_lats, paper_lons = [], []
+        scissors_lats, scissors_lons = [], []
 
-plt.close('all')
+        for i in range(len(species)):
+            if species[i] == 1:
+                rock_lats.append(mlats[i])
+                rock_lons.append(mlons[i])
+            elif species[i] == 2:
+                paper_lats.append(mlats[i])
+                paper_lons.append(mlons[i])
+            elif species[i] == 3:
+                scissors_lats.append(mlats[i])
+                scissors_lons.append(mlons[i])
+
+        ax.plot(rock_lons, rock_lats, marker='o', linestyle='', color='red', ms=2, label='Rocks', transform=vector_crs)
+        ax.plot(paper_lons, paper_lats, marker='o', linestyle='', color='lime', ms=2, label='Papers', transform=vector_crs)
+        ax.plot(scissors_lons, scissors_lats, marker='o', linestyle='', color='cyan', ms=2, label='Scissors', transform=vector_crs)
+
+        plt.title(str(t))
+        ax.legend()
+
+        png_filename = "microbe_warfare_dn" + str(tpd*day + n).zfill(5) + ".png"
+        print("Saving figure: {:s}".format(png_filename))
+        plt.savefig(png_filename, dpi=300, format='png', transparent=False)
+
+        plt.close('all')

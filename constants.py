00547e23a5acebf4f319a07f116249ffa1917d5d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-06 19:55:38 -0500

    Moved grid consts into constants.

--- constants.py
+++ constants.py
@@ -2,8 +2,16 @@ from datetime import datetime, timedelta
 
 output_dir = "rps_proof_of_concept"
 
-N = 28**2  # number of microbes
+lon_min, lon_max = -170, -130
+lat_min, lat_max = 10, 50
+
+# N = 28**2  # number of microbes
+Tx, Ty = 4, 7  # number of "tiles" in the x and y.
+NTx, NTy = 5, 5  # number of microbes in each tile (x and y directions)
+N = Tx*Ty*NTx*NTy  # number of microbes
+
 t = datetime(2017, 1, 1)  # initial time
 dt = timedelta(hours=1)  # advection time step
-tpd = 24  # time steps per day
+tpd = int(timedelta(days=1) / dt)   # time steps per day
+
 n_days = 7  # number of days to advect microbes for

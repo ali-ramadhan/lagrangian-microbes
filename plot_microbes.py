9a1e0b0c3629c161cbd77626acee379fa52c63aa
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-13 09:36:07 -0500

    Better plots for very high particle density.

--- plot_microbes.py
+++ plot_microbes.py
@@ -11,19 +11,21 @@ import matplotlib
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+from matplotlib.patches import Patch
 import cartopy
 import cartopy.util
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
+from constants import output_dir
 from constants import N, t, dt, tpd, n_periods
 from utils import closest_hour
 
-def plot_microbe_warfare_frame(fname):
-    period = int(re.search("p\d\d\d\d", fname).group(0)[1:])
-    hour = int(re.search("h\d\d\d", fname).group(0)[1:])
+def plot_microbe_warfare_frame(fpath):
+    period = int(re.search("p\d\d\d\d", fpath).group(0)[1:])
+    hour = int(re.search("h\d\d\d", fpath).group(0)[1:])
 
-    print("Plotting figure for p={:d}, h={:}...".format(period, hour))
+    print("Plotting figure from {:s}...".format(fpath))
 
     velocity_dataset_filepath = '/home/alir/nobackup/data/oscar_third_deg_180/oscar_vel2017_180.nc'
     velocity_dataset = xr.open_dataset(velocity_dataset_filepath)
@@ -34,7 +36,7 @@ def plot_microbe_warfare_frame(fname):
 
     velocity_subdataset = velocity_dataset.sel(time=t_start, year=year_float, depth=depth_float,
         latitude=slice(60, 0), longitude=slice(-180, -120))
-    
+
     # Grid longitudes and latitudes.
     glons = velocity_subdataset['longitude'].values
     glats = velocity_subdataset['latitude'].values
@@ -44,8 +46,8 @@ def plot_microbe_warfare_frame(fname):
     v_data = velocity_subdataset['v'].values
 
     u_magnitude = np.sqrt(u_data*u_data + v_data*v_data)
-        
-    with open(fname, "rb") as f:
+
+    with open(fpath, "rb") as f:
         microbes = pickle.load(f)
 
     # Microbe longitudes and latidues.
@@ -94,24 +96,30 @@ def plot_microbe_warfare_frame(fname):
             scissors_lats.append(mlats[i])
             scissors_lons.append(mlons[i])
 
-    ax.scatter(rock_lons, rock_lats, marker='o', color='red', s=0.2, label='Rocks', transform=vector_crs)
-    ax.scatter(paper_lons, paper_lats, marker='o', color='limegreen', s=0.2, label='Papers', transform=vector_crs)
-    ax.scatter(scissors_lons, scissors_lats, marker='o', color='blue', s=0.2, label='Scissors', transform=vector_crs)
-    # ax.plot(rock_lons, rock_lats, marker='.', linestyle='', fillstyle='full', color='red', ms=0.2, label='Rocks', transform=vector_crs)
-    # ax.plot(paper_lons, paper_lats, marker='.', linestyle='', fillstyle='full', color='lime', ms=0.2, label='Papers', transform=vector_crs)
-    # ax.plot(scissors_lons, scissors_lats, marker='.', linestyle='', fillstyle='full', color='cyan', ms=0.2, label='Scissors', transform=vector_crs)
+    ms = matplotlib.markers.MarkerStyle(marker=".", fillstyle="full")
+    ax.scatter(rock_lons, rock_lats, marker=ms, linewidths=0, c="red", edgecolors="red", facecolors="red", s=1, label='Rocks', transform=vector_crs)
+    ax.scatter(paper_lons, paper_lats, marker=ms, linewidths=0, color="limegreen", facecolor="limegreen", s=1, label='Papers', transform=vector_crs)
+    ax.scatter(scissors_lons, scissors_lats, marker=ms, linewidths=0, color="blue", edgecolor="blue", facecolor="blue", s=1, label='Scissors', transform=vector_crs)
+    # ax.plot(rock_lons, rock_lats, marker='.', linestyle='', linewidth=0, color="red", markeredgecolor="red", markerfacecolor="red", ms=0.5, label='Rocks', transform=vector_crs)
+    # ax.plot(paper_lons, paper_lats, marker='.', linestyle='', linewidth=0, color="limegreen", markeredgecolor="limegreen", markerfacecolor="limegreen", ms=0.5, label='Papers', transform=vector_crs)
+    # ax.plot(scissors_lons, scissors_lats, marker='.', linestyle='', linewidth=0, color="blue", markeredgecolor="blue", markerfacecolor="blue", ms=0.5, label='Scissors', transform=vector_crs)
 
     plt.title(closest_hour(t_start) + hour*dt)
-    ax.legend()
 
-    png_filename = "microbe_warfare_ph" + str(period).zfill(4) + str(hour).zfill(3) + ".png"
-    print("Saving figure: {:s}".format(png_filename))
-    plt.savefig(png_filename, dpi=300, format='png', transparent=False)
+    rock_patch = Patch(color="red", label="Rocks")
+    paper_patch = Patch(color="limegreen", label="Papers")
+    scissor_patch = Patch(color="blue", label="Scissors")
+    ax.legend(handles=[rock_patch, paper_patch, scissor_patch])
+
+    png_fname = "microbe_warfare_ph" + str(period).zfill(4) + str(hour).zfill(3) + ".png"
+    png_fpath = os.path.join(output_dir, png_fname)
+    print("Saving figure: {:s}".format(png_fpath))
+    plt.savefig(png_fpath, dpi=300, format='png', transparent=False)
 
     plt.close('all')
 
 def renumber_files():
-    frame_files = glob.glob("*.png")
+    frame_files = glob.glob(output_dir + "/microbe_warfare_ph*.png")
     nfiles = len(frame_files)
 
     print("Renaming {:d} files...".format(nfiles))
@@ -120,15 +128,19 @@ def renumber_files():
     new_i = 0
     while new_i < nfiles:
         fname = "microbe_warfare_ph" + str(old_i).zfill(7) + ".png"
-        if os.path.isfile(fname):
+        fpath = os.path.join(output_dir, fname)
+        if os.path.isfile(fpath):
             new_fname = "microbe_warfare_ph" + str(new_i).zfill(7) + ".png"
-            os.rename(fname, new_fname)
-            print("Rename: {:s} -> {:s}".format(fname, new_fname))
+            new_fpath = os.path.join(output_dir, new_fname)
+            os.rename(fpath, new_fpath)
+            print("Rename: {:s} -> {:s}".format(fpath, new_fpath))
             new_i += 1
         old_i += 1
 
 if __name__ == "__main__":
     print("Found {:d} CPUs.".format(joblib.cpu_count()))
-    interaction_files = glob.glob("rps_microbe_species*.pickle")
+    print("Globbing files from {:s}... ".format(output_dir), end="")
+    interaction_files = glob.glob(output_dir + "/rps_microbe_species*.pickle")
+    print("{:d} files found.".format(len(interaction_files)))
     joblib.Parallel(n_jobs=-1)(joblib.delayed(plot_microbe_warfare_frame)(f) for f in interaction_files)
     renumber_files()

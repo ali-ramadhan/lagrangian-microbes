00f8a1bf5ce164eb81121db23a48d457a30d112d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-12-01 23:48:05 -0500

    Splitting up all the output dirs.

--- constants.py
+++ constants.py
@@ -1,16 +1,25 @@
 import os
 from datetime import datetime, timedelta
 
-output_dir = os.path.join("/home", "alir", "nobackup", "lagrangian_microbe_output", "rps_small_patch_490kp")
+OUTPUT_ROOT_DIR = os.path.join("/home", "alir", "nobackup", "lagrangian_microbe_output")
 
-# lon_min, lon_max = -170, -130
-# lat_min, lat_max = 10, 50
-lon_min, lon_max = -155, -145
-lat_min, lat_max = 25, 35
+ADVECTION_OUTPUT_DIR = os.path.join(OUTPUT_ROOT_DIR, "debug_2.8kp_advection")
+INTERACTION_OUTPUT_DIR = os.path.join(OUTPUT_ROOT_DIR, "debug_2.8kp_p0.9_interactions")
+PLOTS_OUTPUT_DIR = os.path.join(OUTPUT_ROOT_DIR, "debug_2.8kp_p0.9_plots")
+
+# ADVECTION_OUTPUT_DIR = os.path.join(OUTPUT_DIR, "small_patch_490kp_advection")
+# INTERACTION_OUTPUT_DIR = os.path.join(OUTPUT_ROOT_DIR, "small_patch_490kp_p0.9_interactions")
+# PLOTS_OUTPUT_DIR = os.path.join(OUTPUT_ROOT_DIR, "small_patch_490kp_p0.9_plots")
+
+lon_min, lon_max = -170, -130
+lat_min, lat_max = 10, 50
+
+# lon_min, lon_max = -155, -145
+# lat_min, lat_max = 25, 35
 
 # N = 28**2  # number of microbes
 Tx, Ty = 4, 7  # number of "tiles" in the x and y.
-NTx, NTy = 100, 175  # number of microbes in each tile (x and y directions)
+NTx, NTy = 10, 10  # number of microbes in each tile (x and y directions)
 N = Tx*Ty*NTx*NTy  # number of microbes
 
 t = datetime(2017, 1, 1)  # initial time
@@ -19,6 +28,9 @@ tpd = int(timedelta(days=1) / dt)   # time steps per day
 
 n_periods = 72  # number of periods to advect microbes for
 
-INTERACTION_LENGTH_SCALE = 0.005  # degrees lol...
+# degrees lol...
+INTERACTION_LENGTH_SCALE = 0.5
+# INTERACTION_LENGTH_SCALE = 0.005
+
 INTERACTION_NORM = 2
 INTERACTION_p = 0.9

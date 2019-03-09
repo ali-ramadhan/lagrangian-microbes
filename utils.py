56fb32f93e6b8e2aeed757675b6b0b9cde9cc09c
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-09 11:45:50 -0500

    Most symmetric integer factorization.

--- utils.py
+++ utils.py
@@ -1,6 +1,44 @@
 from datetime import datetime, timedelta
 
 import numpy as np
+from numpy import sqrt, abs, argmin
+
+
+def factor(n):
+    """
+    Calculates the integer factors of n.
+
+    Args:
+        n: integer to be factored.
+
+    Returns: A sorted list of the factors of n.
+
+    """
+    factors = set()
+    for x in range(1, int(sqrt(n)) + 1):
+        if n % x == 0:
+            factors.add(x)
+            factors.add(n // x)
+    return sorted(factors)
+
+
+def most_symmetric_integer_factorization(N):
+    """
+    Calculates the "most symmetric" integer factorization of N. This is the closest two integer factors of N.
+
+    Args:
+        N: Integer
+
+    Returns: Tuple containing the two "most symmetric" integer factorization of N.
+
+    """
+    factors = np.array(factor(N))
+
+    # Get in the index of the largest factor that is still smaller than sqrt(N).
+    i = argmin(abs(factors - sqrt(N)))
+
+    return factors[i], N // factors[i]
+
 
 def closest_hour(ndt):
     sdt = str(ndt.astype('datetime64[s]'))  # string datetime
@@ -13,14 +51,13 @@ def closest_hour(ndt):
 
     return pdt
 
+
 def runtime2str(t):
-    s = ""
     if t < 1e-6:
-        s = "{:.3g} ns".format(t * 1e9)
+        return "{:.3g} ns".format(t * 1e9)
     elif 1e-6 <= t < 1e-3:
-        s = "{:.3g} μs".format(t * 1e6)
+        return "{:.3g} μs".format(t * 1e6)
     elif 1e-3 < t < 1:
-        s = "{:.3g} ms".format(t * 1e3)
+        return "{:.3g} ms".format(t * 1e3)
     else:
-        s = "{:.3g} s".format(t)
-    return s
+        return "{:.3g} s".format(t)

348a07b030acedf44907716de2db779a67736f94
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-29 09:26:20 -0400

    Added output dir option to args

--- rock_paper_scissors_argparse.py
+++ rock_paper_scissors_argparse.py
@@ -16,13 +16,14 @@ parser = argparse.ArgumentParser(description="Simulate some Lagrangian microbes 
 
 parser.add_argument("-N", "--N_particles", type=int, nargs=1, required=True, help="Number of Lagrangian microbes")
 parser.add_argument("-p", type=float, nargs=1, required=True, help="Interaction probability")
+parser.add_argument("-d", "--output_dir", type=str, nargs=1, required=True, help="Output directory")
 
 args = parser.parse_args()
-N, p = args.N_particles[0], args.p[0]
+N, p, base_dir = args.N_particles[0], args.p[0], args.output_dir[0]
 pRS, pPR, pSP = p, p, p
 
 # Output directories.
-base_output_dir = os.path.join("/home/alir/cnhlab004/lagrangian_microbes_output", "N" + str(N) + "_" + "p" + str(p))
+base_output_dir = os.path.join(base_dir, "N" + str(N) + "_" + "p" + str(p))
 advection_output_dir = os.path.join(base_output_dir, "particle_locations")
 interaction_output_dir = os.path.join(base_output_dir, "microbe_interactions")
 plots_output_dir = os.path.join(base_output_dir, "plots")
@@ -35,7 +36,7 @@ dt = timedelta(hours=1)
 particle_lons, particle_lats = uniform_particle_locations(N_particles=N, lat_min=20, lat_max=50, lon_min=198, lon_max=208)
 
 # Create a particle advecter that will the advect the particles we just generated in parallel.
-pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=8, velocity_field="OSCAR", output_dir=advection_output_dir)
+pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=4, velocity_field="OSCAR", output_dir=advection_output_dir)
 
 # Advect the particles.
 pa.time_step(start_time=start_time, end_time=end_time, dt=dt)
@@ -50,5 +51,5 @@ isim.time_step(start_time=start_time, end_time=end_time, dt=dt)
 # Create a microbe plotter that will produce a plot of all the microbes at a single iteration.
 mp = MicrobePlotter(N_procs=8, dark_theme=True, input_dir=interaction_output_dir, output_dir=plots_output_dir)
 
-# Plot the first 100 frames and save them to disk.
-mp.plot_frames(0, isim.iteration)
+# Plot all the frames.
+mp.plot_frames(0, 100)

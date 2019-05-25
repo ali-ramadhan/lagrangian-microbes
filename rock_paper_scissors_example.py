182339f1f829f65cacbc33507ec4d436e3d77b45
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-05-25 13:44:14 -0400

    Saving to NetCDF in example.

--- rock_paper_scissors_example.py
+++ rock_paper_scissors_example.py
@@ -21,18 +21,19 @@ particle_lons, particle_lats = uniform_particle_locations(N_particles=N, lat_min
 # Create a particle advecter that will the advect the particles we just generated on 4 processors.
 pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=4, velocity_field="OSCAR", output_dir=output_dir)
 
-# Advect the particles.
-pa.time_step(start_time=start_time, end_time=end_time, dt=dt)
+# Advect the particles and save all the data to NetCDF.
+pa.time_step(start_time, end_time, dt)
+pa.create_netcdf_file(start_time, end_time, dt)
 
-# Create an interaction simulator that uses the rock-paper-scissors pair interaction.
-rps_interaction = rock_paper_scissors(N_microbes=N, pRS=0.5, pPR=0.5, pSP=0.5)
-isim = InteractionSimulator(pa, pair_interaction=rps_interaction, interaction_radius=0.05, output_dir=output_dir)
-
-# Simulate the interactions.
-isim.time_step(start_time=start_time, end_time=end_time, dt=dt)
-
-# Create a microbe plotter that will produce a plot of all the microbes at a single iteration.
-mp = MicrobePlotter(N_procs=1, dark_theme=True, input_dir=output_dir, output_dir=output_dir)
-
-# Plot the first 100 frames and save them to disk.
-mp.plot_frames(0, 100)
+# # Create an interaction simulator that uses the rock-paper-scissors pair interaction.
+# rps_interaction = rock_paper_scissors(N_microbes=N, pRS=0.5, pPR=0.5, pSP=0.5)
+# isim = InteractionSimulator(pa, pair_interaction=rps_interaction, interaction_radius=0.05, output_dir=output_dir)
+#
+# # Simulate the interactions.
+# isim.time_step(start_time=start_time, end_time=end_time, dt=dt)
+#
+# # Create a microbe plotter that will produce a plot of all the microbes at a single iteration.
+# mp = MicrobePlotter(N_procs=1, dark_theme=True, input_dir=output_dir, output_dir=output_dir)
+#
+# # Plot the first 100 frames and save them to disk.
+# mp.plot_frames(0, 100)

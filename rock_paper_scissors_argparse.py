d5aa95aac4045063145d1531908f87cc9fe2a8d3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-05-26 09:57:00 -0400

    Update RPS argparse script with Kh option

--- rock_paper_scissors_argparse.py
+++ rock_paper_scissors_argparse.py
@@ -18,19 +18,17 @@ from interactions import rock_paper_scissors
 
 parser = argparse.ArgumentParser(description="Simulate some Lagrangian microbes in the Northern Pacific.")
 
-parser.add_argument("-N", "--N-particles", type=int, nargs=1, required=True, help="Number of Lagrangian microbes")
-parser.add_argument("-p", type=float, nargs=1, required=True, help="Interaction probability")
-parser.add_argument("-d", "--output_dir", type=str, nargs=1, required=True, help="Output directory")
+parser.add_argument("-N", "--N-particles", type=int, required=True, help="Number of Lagrangian microbes")
+parser.add_argument("-K", "--Kh", type=float, required=True, help="Isotropic horizontal diffusivity")
+parser.add_argument("-p", type=float, required=True, help="Interaction probability")
+parser.add_argument("-d", "--output_dir", type=str, required=True, help="Output directory")
 
 args = parser.parse_args()
-N, p, base_dir = args.N_particles[0], args.p[0], args.output_dir[0]
+N, Kh, p, base_dir = args.N_particles, args.Kh, args.p, args.output_dir
 pRS, pPR, pSP = p, p, p
 
 # Output directories.
-base_output_dir = os.path.join(base_dir, "N" + str(N) + "_" + "p" + str(p))
-advection_output_dir = os.path.join(base_output_dir, "particle_locations")
-interaction_output_dir = os.path.join(base_output_dir, "microbe_interactions")
-plots_output_dir = os.path.join(base_output_dir, "plots")
+output_dir = os.path.join(base_dir, "N" + str(N) + "_Kh" + str(Kh) + "_p" + str(p))
 
 start_time = datetime(2018, 1, 1)
 end_time = datetime(2018, 2, 1)
@@ -40,20 +38,32 @@ dt = timedelta(hours=1)
 particle_lons, particle_lats = uniform_particle_locations(N_particles=N, lat_min=20, lat_max=50, lon_min=198, lon_max=208)
 
 # Create a particle advecter that will the advect the particles we just generated in parallel.
-# pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=4, velocity_field="OSCAR", output_dir=advection_output_dir)
+pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=4, velocity_field="OSCAR", output_dir=advection_output_dir)
 
-# Advect the particles.
-# pa.time_step(start_time=start_time, end_time=end_time, dt=dt)
+# Advect the particles and save all the data to NetCDF.
+pa.time_step(start_time, end_time, dt)
+pa.create_netcdf_file(start_time, end_time, dt)
 
 # Create an interaction simulator that uses the rock-paper-scissors pair interaction.
-rps_interaction = rock_paper_scissors(N_microbes=N, pRS=pRS, pPR=pPR, pSP=pSP)
-# isim = InteractionSimulator(pa, pair_interaction=rps_interaction, interaction_radius=0.05, output_dir=interaction_output_dir)
+rps_interaction = rock_paper_scissors(N_microbes=N, pRS=0.5, pPR=0.5, pSP=0.5)
+isim = InteractionSimulator(pair_interaction=rps_interaction, interaction_radius=0.05, output_dir=output_dir)
 
 # Simulate the interactions.
-# isim.time_step(start_time=start_time, end_time=end_time, dt=dt)
+isim.time_step(start_time, end_time, dt)
 
 # Create a microbe plotter that will produce a plot of all the microbes at a single iteration.
-mp = MicrobePlotter(N_procs=1, dark_theme=True, input_dir=interaction_output_dir, output_dir=plots_output_dir)
+mp = MicrobePlotter(N_procs=-1, dark_theme=True, input_dir=output_dir, output_dir=output_dir)
 
-# Plot all the frames.
-mp.plot_frames(0, 10)
+# Plot the first 100 frames and save them to disk.
+mp.plot_frames(start_time, end_time, dt)
+
+# Make movie!
+(
+    ffmpeg
+    .input(os.path.join(output_dir, "lagrangian_microbes_%05d.png"), framerate=30)
+    .output(os.path.join(output_dir, "movie.mp4"), crf=15, pix_fmt='yuv420p')
+    .run()
+)
+
+for fl in glob.glob(os.path.join(output_dir, "*.png")):
+    os.remove(fl)

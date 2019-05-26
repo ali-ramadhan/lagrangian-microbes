a78b5dde146afb19b1b767841642d0584c346b08
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-05-26 11:21:05 -0400

    Forgot to include Kh in script

--- rock_paper_scissors_argparse.py
+++ rock_paper_scissors_argparse.py
@@ -41,7 +41,7 @@ dt = timedelta(hours=1)
 particle_lons, particle_lats = uniform_particle_locations(N_particles=N, lat_min=25, lat_max=35, lon_min=205, lon_max=215)
 
 # Create a particle advecter that will the advect the particles we just generated in parallel.
-pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=25, velocity_field="OSCAR", output_dir=output_dir)
+pa = ParticleAdvecter(particle_lons, particle_lats, N_procs=25, velocity_field="OSCAR", output_dir=output_dir, Kh=Kh)
 
 # Advect the particles and save all the data to NetCDF.
 pa.time_step(start_time, end_time, dt)

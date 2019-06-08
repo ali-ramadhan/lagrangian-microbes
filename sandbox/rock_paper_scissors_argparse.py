c4d78090511edd3657d48a578f071269529bfb7d
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-08 10:26:52 -0400

    Rock paper scissors advection script

--- sandbox/rock_paper_scissors_argparse.py
+++ sandbox/rock_paper_scissors_argparse.py
@@ -7,12 +7,6 @@ import numpy as np
 from numpy import int8
 import ffmpeg
 
-# # Configure logger first before importing any sub-module that depend on the logger being already configured.
-# import logging.config
-#
-# logging.config.fileConfig("logging.ini")
-# logger = logging.getLogger(__name__)
-
 from particle_advecter import ParticleAdvecter, uniform_particle_locations
 from interactions import rock_paper_scissors
 from interaction_simulator import InteractionSimulator

8d2a9d8f9b2844a7ffef9e11771643666e683c6b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-08 13:11:06 -0400

    Improving slurm advect

--- sandbox/slurm_advect.py
+++ sandbox/slurm_advect.py
@@ -1,9 +1,4 @@
-import logging
-
 from subprocess import Popen
-
-logging.basicConfig(level=logging.INFO, format="[%(asctime)s.%(msecs)03d] %(funcName)s:%(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-logger = logging.getLogger(__name__)
 
 MODULE_CMD = "module add engaging/ffmpeg/20160616 engaging/x264/20160616"
 
@@ -21,12 +16,8 @@ def rps_advect_cmd(C, N, Kh, dir):
            + " -d " + str(dir)
 
 
-if __name__ == "__main__":
-    C = 25
-    N = 10000
-    Kh = 33
-
-    script_lines = \
+def slurm_script_lines(C, N, Kh, hours):
+    lines = \
         ["#!/bin/bash",
          "#",
          "#SBATCH --job-name=advect_microbes_N{:d}_Kh{:d}".format(N, Kh),
@@ -37,15 +28,30 @@ if __name__ == "__main__":
          "#SBATCH --nodes=1",
          "#SBATCH --ntasks=1",
          "#SBATCH --cpus-per-task=28",
-         "#SBATCH --time=60:00",
+         "#SBATCH --time={:d}:00:00".format(hours),
          "#SBATCH --mem=100gb",
          "",
          CD_CMD,
          ENV_CMD,
          rps_advect_cmd(C, N, Kh, OUTPUT_DIR)]
 
-    slurm_script_filename = "advect_rps_N{:d}_Kh{:d}.slurm".format(N, Kh)
-    with open(slurm_script_filename, "w") as f:
-        f.writelines("{:s}\n".format(l) for l in script_lines)
+    return lines
 
-    p = Popen("sbatch {:s}".format(slurm_script_filename), shell=True)
+
+if __name__ == "__main__":
+    hours = {
+        10000: 1,
+        100000: 3,
+        1000000: 12
+    }
+
+    C = 25
+    for N in [1000000]:
+        for Kh in [0, 20, 100, 500]:
+            slurm_script_filename = "advect_rps_N{:d}_Kh{:d}.slurm".format(N, Kh)
+
+            with open(slurm_script_filename, "w") as f:
+                script_lines = slurm_script_lines(C, N, Kh, hours[N])
+                f.writelines("{:s}\n".format(l) for l in script_lines)
+
+            p = Popen("sbatch {:s}".format(slurm_script_filename), shell=True)
